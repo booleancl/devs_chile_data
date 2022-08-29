72b5ed040a365c45f8aa69ba84f7574580c7326d
Gonzalo Pincheira A <g.pincheira.a@gmail.com>
2022-08-29 19:41:37 -0400

    update(cors): adding cors

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
 
 app.use('/', indexRouter)
 app.use('/answers', answersRouter)
